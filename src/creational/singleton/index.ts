9664b84979940037773d9db6011cc089820d9bc3
jhnmarlen <johnmarlen@fadat.edu.br>
2022-04-20 17:21:47 -0300

    refactor (Singleton): refatoração de código

---
+++ src/creational/singleton/index.ts
@@ -0,0 +1,17 @@
+import { Singleton } from './Singleton';
+
+const instance1: Singleton = Singleton.instance;
+instance1.appendNumberInList(1);
+instance1.appendNumberInList(3);
+instance1.appendNumberInList(5);
+instance1.appendNumberInList(7);
+
+const instance2: Singleton = Singleton.instance;
+instance2.appendNumberInList(2);
+instance2.appendNumberInList(4);
+instance2.appendNumberInList(6);
+instance2.appendNumberInList(8);
+
+//Ambas as saídas trarão a mesma lista com todos os números de 1 a 8
+console.log(instance1.list);
+console.log(instance2.list);
