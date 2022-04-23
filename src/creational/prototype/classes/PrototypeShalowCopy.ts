1364704b3c6bca158d5f117fc62699cb590261f4
QuodDG <jhnmarlenn@gmail.com>
2022-04-23 15:12:34 -0300

    feat (Prototype): Finalização do padrão Prototype

---
+++ src/creational/prototype/classes/PrototypeShalowCopy.ts
@@ -0,0 +1,73 @@
+import { Prototype } from '../contracts/PersonPrototypeContract';
+
+export class Person {
+	private _name: string;
+
+	constructor(name: string) {
+		this._name = name;
+	}
+
+	public get name(): string {
+		return this._name;
+	}
+}
+
+export class Driver extends Person {
+	private _licenseNumber: string;
+
+	constructor(name: string, licenseNumber: string) {
+		super(name);
+		this._licenseNumber = licenseNumber;
+	}
+}
+
+export class Car implements Prototype {
+	private _plate: string;
+	private _model: string;
+	private _color: string;
+	private _driver: Driver | null;
+
+	constructor(plate: string, model: string, color: string) {
+		this._plate = plate;
+		this._model = model;
+		this._color = color;
+		this._driver = null;
+	}
+
+	public get plate(): string {
+		return this._plate;
+	}
+
+	public set plate(plate: string) {
+		this._plate = plate;
+	}
+
+	public get model(): string {
+		return this._model;
+	}
+
+	public set model(model: string) {
+		this._model = model;
+	}
+
+	public get color(): string {
+		return this._color;
+	}
+
+	public set color(color: string) {
+		this._color = color;
+	}
+
+	public get driver(): Driver | null {
+		return this._driver;
+	}
+
+	public attachDriver(driver: Driver): void {
+		this._driver = driver;
+	}
+
+	public clone(): Car {
+		const clonedCar = Object.create(this);
+		return clonedCar;
+	}
+}
