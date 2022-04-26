2d5d71d53375c8558c62bb56a47963b243df5b5d
QuodDG <jhnmarlenn@gmail.com>
2022-04-26 11:13:30 -0300

    Saving files before refreshing line endings

--- src/creational/factory-method/classes/vehicles/Car.ts
+++
@@ -1,7 +0,0 @@
-import { Vehicle } from '../Vehicle';
-
-export class Car extends Vehicle {
-	constructor(plate: string, model: string, color: string) {
-		super(plate, model, color, 4);
-	}
-}
--- src/creational/factory-method/classes/vehicles/Motorcycle.ts
+++
@@ -1,7 +0,0 @@
-import { Vehicle } from '../Vehicle';
-
-export class Motorcycle extends Vehicle {
-	constructor(plate: string, model: string, color: string) {
-		super(plate, model, color, 1);
-	}
-}
